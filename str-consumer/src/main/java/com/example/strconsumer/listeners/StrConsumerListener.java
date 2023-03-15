e27d34f7798244eadb9da76956c5116462baab3f
Mario Franco <mario.franco@wdev.int>
2023-03-15 19:00:46 -0300

    finish part 1 of study

--- str-consumer/src/main/java/com/example/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/example/strconsumer/listeners/StrConsumerListener.java
@@ -1,6 +1,8 @@
 package com.example.strconsumer.listeners;
 
 
+import com.example.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -10,8 +12,20 @@ import org.springframework.stereotype.Component;
 public class StrConsumerListener {
 
 
-    @KafkaListener(groupId = "group-1",topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Receive message", message);
+    @SneakyThrows
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+        throw new IllegalArgumentException("EXCEPTION...");
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
     }
 }
