791ca8950e9d24868358dba177d8195c1d001b1b
Mario Franco <mario.franco@wdev.int>
2023-03-16 10:12:23 -0300

    add producer config

--- payment-service/src/main/java/com/mario/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/mario/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,28 @@ package com.mario.paymentservice.service.impl;
 
 import com.mario.paymentservice.model.Payment;
 import com.mario.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_IMPL ::: Recebi pagamento {}", payment);
+        log.info("Recebi pagamento {}", payment);
+        Thread.sleep(1000);
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic",payment);
     }
 }
